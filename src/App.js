a828a2d03b0409188f53e512038274e7040e15cc
itsanasna <nahil.anas1@gmail.com>
2021-08-11 14:53:45 -0400

    changed parojects to use Card from MDBootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Components/Home'
 import NavBar from './Components/NavBar'
 import AboutMe from './Components/AboutMe'
 import Projects from './Components/Projects'
+import './App.css'
 
 
 
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,73 +1,135 @@
 import React from "react";
-import Carousel from "react-bootstrap/Carousel";
 import SnowmanGame from "../Images/SnowmanGame.gif";
 import textadventure from "../Images/textadventure.gif";
+import FauxTube from "../Images/FauxTube.jpg";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBBtn,
+  MDBRipple,
+  MDBRow,
+  MDBCol,
+} from "mdb-react-ui-kit";
 import "./Projects.css";
 
 const Projects = () => {
   return (
-    <div className="projects">
-      <h1>Projects</h1>
-        <Carousel
-          fade
-          interval={null}
-          className="carousel "
-          data-bs-touch="false"
-        >
-          <Carousel.Item>
-            <div class="d-flex justify-content-center ">
-              <img
-                className="d-block w-70"
-                src={SnowmanGame}
-                alt="First slide"
-              />
-            </div>
-            <Carousel.Caption>
-              <h3 class="text-danger">Snowman</h3>
-              <p>
-                {" "}
+    <div className="prjectsPage">
+      <MDBRow className="row-cols-1 row-cols-sm-2 row-cols-md-3 mt-3">
+        <MDBCol>
+          <MDBCard
+            style={{ maxWidth: "22rem" }}
+            alignment="center"
+            className="h-100"
+          >
+            <MDBRipple
+              rippleColor="light"
+              rippleTag="div"
+              className="bg-image hover-overlay"
+            >
+              <MDBCardImage src={SnowmanGame} fluid alt="Snowman Project" />
+              <a>
+                <div
+                  className="mask"
+                  style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
+                ></div>
+              </a>
+            </MDBRipple>
+            <MDBCardBody>
+              <MDBCardTitle>Snowman Game:</MDBCardTitle>
+              <MDBCardText>
                 As the player in The Snowman Game, you get presented a masked
                 word that you have to guess in less than 7 tries.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <div class="d-flex justify-content-center">
-              <img
-                className="d-block w-70"
-                src={textadventure}
-                alt="Second slide"
-              />
-            </div>
-            <Carousel.Caption>
-              <h3>The HyruleText Adventure</h3>
-              <p>
-                In The Hyrule Adventure Game -inspired by the popular game: The
-                Legend Of Zelda-, you play as the hero Link saving Princess
-                Zelda. by entering text or numbers to answer simple questions,
-                you're able to navigate through The Hyrule Castle to find Zelda.
-                Choose wisely to succesfully save the princess.
-              </p>
-            </Carousel.Caption>
-            <button>click me</button>
-          </Carousel.Item>
-          <Carousel.Item>
-            <div class="d-flex justify-content-center">
-              <img
-                className="d-block w-90"
-                src="holder.js/800x400?text=Third slide&bg=20232a"
-                alt="Third slide"
-              />
-            </div>
+              </MDBCardText>
+              <MDBBtn
+                href="https://github.com/itsanasna/FSW-CLI-Snowman"
+                target="_blank"
+              >
+                GitHub Code
+              </MDBBtn>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
 
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+        <MDBCol>
+          <MDBCard
+            style={{ maxWidth: "22rem" }}
+            alignment="center"
+            className="h-100"
+          >
+            <MDBRipple
+              rippleColor="light"
+              rippleTag="div"
+              className="bg-image hover-overlay"
+            >
+              <MDBCardImage
+                src={textadventure}
+                fluid
+                alt="Text Based Adventure"
+              />
+              <a>
+                <div
+                  className="mask"
+                  style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
+                ></div>
+              </a>
+            </MDBRipple>
+            <MDBCardBody>
+              <MDBCardTitle>The Hyrule Text Adventure</MDBCardTitle>
+              <MDBCardText>
+                In The Hyrule Adventure Game inspired by the popular game: The
+                Legend Of Zelda, you play as the hero Link saving PrincessZelda.
+                by entering text or numbers to answer simple questions,and
+                navigate through the castle to find Zelda.
+              </MDBCardText>
+              <MDBBtn
+                href="https://github.com/itsanasna/FSW-Text-Based-Adventure"
+                target="_blank"
+              >
+                GitHub Code
+              </MDBBtn>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+
+        <MDBCol>
+          <MDBCard
+            style={{ maxWidth: "22rem" }}
+            alignment="center"
+            className="h-100"
+          >
+            <MDBRipple
+              rippleColor="light"
+              rippleTag="div"
+              className="bg-image hover-overlay"
+            >
+              <MDBCardImage src={FauxTube} fluid alt="..." />
+              <a>
+                <div
+                  className="mask"
+                  style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
+                ></div>
+              </a>
+            </MDBRipple>
+            <MDBCardBody>
+              <MDBCardTitle>FauxTube:</MDBCardTitle>
+              <MDBCardText>
+                As the player in The Snowman Game, you get presented a masked
+                word that you have to guess in less than 7 tries.
+              </MDBCardText>
+              <MDBBtn
+                href="https://github.com/itsanasna/FSW-CLI-Snowman"
+                target="_blank"
+              >
+                GitHub Code
+              </MDBBtn>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
     </div>
   );
 };
